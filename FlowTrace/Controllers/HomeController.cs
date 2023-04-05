76feca04236a2f6bb52cf9ebbd097a0d52fd552e
Zach Hardin <hardinzw@gmail.com>
2023-04-05 14:02:02 -0400

    Add project files.

---
+++ FlowTrace/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using FlowTrace.Areas.Identity.Data;
+using FlowTrace.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FlowTrace.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FlowTrace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlowTrace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
